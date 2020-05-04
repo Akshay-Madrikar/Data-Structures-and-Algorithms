9d7371d8eb4dd3cd187b44692d30279118aab81f
Akshay Madrikar <akshaymadrikar@gmail.com>
2020-05-04 20:07:19 +0530

    Algorithm updated

--- Arrays/maximumSubArray.js
+++ Arrays/maximumSubArray.js
@@ -3,18 +3,34 @@ const maximumSubArray = (arr) => {
     if( arr.length === 0 || arr === null ){
         return 0;
     }
-    
+
     let sum = arr[0];
     let currSum = sum;
+
+    //========= 1 way =====================/
+    // for( let i =1; i<arr.length; i++ ){
+    //     if(currSum < 0 && currSum <= arr[i]){
+    //         currSum = arr[i]
+    //     }else {
+    //         currSum += arr[i]
+    //     }
+    //     sum = Math.max(sum,currSum); 
+    // }
+    // return sum;
+    //========================================/
+
+    //=========== 2nd way ====================/
     for( let i =1; i<arr.length; i++ ){
-        if(currSum < 0 && currSum <= arr[i]){
-            currSum = arr[i]
-        }else {
-            currSum += arr[i]
+        currSum = Math.max(arr[i], currSum + arr[i])
+
+        if( currSum > sum ){
+            sum = currSum;
         }
-        sum = Math.max(sum,currSum); 
     }
     return sum;
+    //=========================================/
+
 };
 
-console.log(maximumSubArray([-2,1,-3,4,-1,2,1,-5,4]))
+console.log(maximumSubArray([-2,1,-3,4,-1,2,1,-5,4])) //6
+console.log(maximumSubArray([10,22,-4,-8,45])) //65
