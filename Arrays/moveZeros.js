ecc9b2d1ebe3f8c4467180267defe61e92cb231a
Akshay Madrikar <akshaymadrikar@gmail.com>
2020-05-06 18:25:08 +0530

    Algorithm updated

---
+++ Arrays/moveZeros.js
@@ -0,0 +1,40 @@
+const moveZeros = (arr) => {
+    
+    if(arr.length === 0 || arr === null) {
+        return 0;
+    }
+
+//======== 1 way =====================/
+    // let pos = 0;
+    // for(let i=0; i<arr.length; i++){
+    //      if(arr[i] !== 0){
+    //          arr[pos] = arr[i];
+    //          pos++
+    //      }
+    // }
+
+    // for(; pos<arr.length; pos++){
+    //     arr[pos] = 0;
+    // }
+    // return arr
+//======================================/
+
+//======== 2 way =====================/
+    let anchor = 0;
+
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] !== 0){
+            let temp = arr[anchor];
+            arr[anchor] = arr[i];
+            arr[i] = temp;
+
+            anchor++
+        }
+    }
+    return arr
+//======================================/
+
+}
+
+console.log(moveZeros([0,1,0,3,12])); //[1, 3, 12, 0, 0]
+console.log(moveZeros([0,5,8,0,20,12])); //[5, 8 ,20, 12, 0, 0]
