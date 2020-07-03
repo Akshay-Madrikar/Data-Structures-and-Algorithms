ad2ffa88597a3792f97cf0f0d4a345bfc987cde6
Akshay Madrikar <akshaymadrikar@gmail.com>
2020-07-03 13:48:46 +0530

    algorithm added

--- Arrays/firstRecurringNumber.js
+++ Arrays/firstRecurringNumber.js
@@ -1,3 +1,4 @@
+//--- This will return 1 rather than 2 ------------
 const recur = (arr) => {
     for(let i=0; i<arr.length; i++) {
         for(let j=i+1; j<arr.length; j++) {
@@ -8,5 +9,18 @@ const recur = (arr) => {
     }
     return undefined;
 }
+console.log(recur([1,2,2,3,1,4])); //1
 
-console.log(recur([1,2,3,1,4]));
+// ----- Alternate solution-------------
+const recur2 = (arr) => {
+    const num = [];
+    for(let i=0; i<arr.length; i++) {
+        if(!num.includes(arr[i])) {
+            num.push(arr[i]);
+        } else {
+            return arr[i]
+        }
+    }
+    return undefined;
+}
+console.log(recur2([1,2,2,3,1,4])); //2
---
+++ Objects/firstRecurringNumber.js
@@ -0,0 +1,14 @@
+const recur = (arr) => {
+    let map = {};
+
+    for(let i=0; i<arr.length; i++) {
+        if(map[arr[i]] !== undefined) {
+            return arr[i];
+        } else {
+            map[arr[i]] = i;
+        }
+    }
+    return undefined;
+}
+
+console.log(recur([1,2,4,1,6,9]))
