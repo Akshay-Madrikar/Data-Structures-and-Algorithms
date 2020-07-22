776756501ab249ee44bf2236780eb79002370601
Akshay Madrikar <akshaymadrikar@gmail.com>
2020-07-22 22:36:38 +0530

    alogorithm added

---
+++ Arrays/maxChars.js
@@ -0,0 +1,24 @@
+const maxChars = (str) => {
+    const charsMap = {};
+    let max = 0; 
+    let maxChar = '';
+
+    for( let char of str ) {
+        if(!charsMap[char]) {
+         charsMap[char] = 1
+        } else {
+         charsMap[char]++
+        }
+    };
+    
+    for( let char in charsMap ) {
+        if (charsMap[char] > max) {
+            max = charsMap[char];
+            maxChar = char;
+        }
+    };
+
+    return maxChar;
+}
+
+console.log(maxChars('Hello There!')) // e
